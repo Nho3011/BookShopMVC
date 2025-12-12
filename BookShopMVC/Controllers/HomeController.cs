beff21420c920764493d710739b57ad45fb74c85
Đặng Tiến Dũng <dangtiendung722@gmail.com>
2025-12-12 22:11:44 +0700

    chore(common): khởi tạo dự án BookShopMVC

---
+++ BookShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using BookShopMVC.DataAccess.Data;
+using BookShopMVC.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _context.Products.ToList();
+            foreach(var hh in list)
+            {
+                _logger.LogInformation(hh.Id + " \n");
+            }
+                
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
