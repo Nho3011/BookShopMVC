8918bef4fbb78806b1baa18fe6fe10394e50addb
Hong_Nhu <hongnhu1505@gmail.com>
2025-12-15 00:44:08 +0700

    fix(category): Chặn quyền User thường truy cập trang Category [#12]

--- BookShopMVC/Areas/Admin/Controllers/CategoryController.cs
+++ BookShopMVC/Areas/Admin/Controllers/CategoryController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BookShopMVC.Areas.Admin.Controllers
 {
 	[Area("Admin")]
-	//[Authorize(Roles = StaticDetails.Role_Admin)]
+	[Authorize(Roles = StaticDetails.Role_Admin)]
 	public class CategoryController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
